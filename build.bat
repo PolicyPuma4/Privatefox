16e0928a852557bc715a3403f89d27875c1cb258
PolicyPuma4 <83885295+PolicyPuma4@users.noreply.github.com>
2023-04-24 21:59:49 +0100

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 cargo.exe build --release --target=i686-pc-windows-msvc
 cargo.exe build --release --target=x86_64-pc-windows-msvc
-"C:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc "%1"
+"C:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc Privatefox.iss
