778a6c92ccb10871ffc809486c26a40db3d438d4
PolicyPuma4 <83885295+PolicyPuma4@users.noreply.github.com>
2023-04-24 22:48:42 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+rustup.exe target add i686-pc-windows-msvc
 cargo.exe build --release --target=i686-pc-windows-msvc
 cargo.exe build --release --target=x86_64-pc-windows-msvc
 "C:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc Privatefox.iss
